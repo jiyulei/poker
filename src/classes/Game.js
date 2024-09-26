9e052645299da07b1658b4dfb5638184c8f8da9a
JiyuLei <jerrylei.ui@gmail.com>
2024-09-26 04:11:04 -0700

    Jiyu | Add dealCommunityCards function

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -8,19 +8,58 @@ class Game {
     this.smallBlind = smallBlind;
     this.bigBlind = bigBlind;
     this.pot = 0;
-    this.inGamePlayers = players;
+    this.players = players;
     this.communityCards = [];
+    this.state = 0; // preflop: 0 , flop: 1, turn: 2, river: 3
   }
 
-  startGame() {}
+  dealHands() {
+    this.players.forEach((player) => {
+      player.receiveCards(this.deck.deal(2));
+    });
+  }
+
+  placeBet() { 
+    // TODO: each players should place bet, then state++
+    this.state += 1;
+  }
+
+  startGame() {
+    this.deck.shuffle();
+    this.dealHands();
+  }
+
+  dealCommunityCards() {
+    if (this.state === 1) {
+      const dealtCards = this.deck.deal(3);
+
+      this.communityCards.push(...dealtCards);
+    } else {
+      const dealtCards = this.deck.deal(1);
+
+      this.communityCards.push(...dealtCards);
+    }
+  }
 }
+
+// -------Test Case-------
 
 const player1 = new Player("lei", 1000);
 const player2 = new Player("clm", 1000);
 const players = [player1, player2];
 
-console.log("players: ", players);
-
-
 const game = new Game(2, 5, 10, players);
-console.log("game: ", game.bigBlind);
+// console.log("game: ", game);
+game.startGame();
+console.log(player1.name + "'s hand: ", player1.hands);
+console.log(player2.name + "'s hand: ", player2.hands);
+
+game.placeBet();
+game.dealCommunityCards();
+console.log("------- common -------", game.communityCards);
+game.placeBet();
+game.dealCommunityCards();
+console.log("------- common -------", game.communityCards);
+game.placeBet();
+game.dealCommunityCards();
+console.log("------- common -------", game.communityCards);
